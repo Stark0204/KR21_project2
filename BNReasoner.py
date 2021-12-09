34bfe642a3a3dfa1203cc2ff032bdf4513cd2981
hendrikctimmer <hendrikctimmer@gmail.com>
2021-12-09 16:52:40 +0100

    summing out done

--- BNReasoner.py
+++ BNReasoner.py
@@ -2,6 +2,7 @@ from typing import Union
 from BayesNet import BayesNet
 import pandas as pd
 import numpy as np
+from copy import deepcopy
 
 
 class BNReasoner:
@@ -17,7 +18,12 @@ class BNReasoner:
         else:
             self.bn = net
 
-    # TODO: This is where your methods should g
+    # TODO: This is where your methods should go
+
+new_net = BayesNet()
+new_net.load_from_bifxml('/home/rik/Desktop/Jong_Master/Knowledge Representation/PGM/KR21_project2/testing/lecture_example2.BIFXML')
+
+variables = []
 
 def marginalise(Q, E):
 		
@@ -25,23 +31,23 @@ def marginalise(Q, E):
 
 	if not E:
 		#If the evidence set is empty, and the probability table of single variable Q already exists, return that table
-		if len(Q) == 1:
-			if Q[0] in new_net.bn.get_all_cpts().keys():
+		if len(Q) == 1 : #and not Q[0].getparents()
+			if Q[0] in new_net.get_all_cpts().keys():
 				print('The table of Q already exists as a lone probability table \n')
-				print(new_net.bn.get_all_cpts().get(Q[0]))
-				return new_net.bn.get_all_cpts().get(Q[0])
+				print(new_net.get_all_cpts().get(Q[0]))
+				return new_net.get_all_cpts().get(Q[0])
 		else:
-			pass
+			sum_out(new_net.get_all_cpts(), Q) #This is a temporary solution as summing out, which works
 
 	else:
 		print('here1')
 		#If E is not empty, but Q is a single variable and the CPT exists, return that table
 		if len(Q) == 1:
 
-			if Q[0] in new_net.bn.get_all_cpts().keys():
+			if Q[0] in new_net.get_all_cpts().keys():
 				
-				for x in new_net.bn.get_all_cpts():
-					index_list = list(new_net.bn.get_all_cpts().get(x).columns)
+				for x in new_net.get_all_cpts():
+					index_list = list(new_net.get_all_cpts().get(x).columns)
 					index_list.remove('p')
 					if index_list[-1] == Q[0]:
 						index_list.remove(Q[0])
@@ -54,54 +60,26 @@ def marginalise(Q, E):
 								index_list.remove(x)
 
 						if E:
-							print('im here')
-							print(new_net.bn.get_all_cpts().get(Q[0]))
-							return(new_net.bn.get_all_cpts().get(Q[0]))
+							print(new_net.get_all_cpts().get(Q[0]))
+							return(new_net.get_all_cpts().get(Q[0]))
 
 
-		
-				#if Q:
-				#	pass
-							
-		
-		#1. check whether the CPT already exists 	DONE
-		#2. if so, return that CPT		 	DONE
-		#3. if not, multiply to find the full CPT	
-		#4. sum out to retrive the PT of P(Q)
-			
-			
+def sum_out(all_tables, query_list):
 
-new_net = BNReasoner('/home/rik/Desktop/Jong_Master/Knowledge Representation/PGM/KR21_project2/testing/lecture_example2.BIFXML')
-for x in new_net.bn.get_all_cpts():
-	#print(new_net.bn.get_all_cpts().get(x))
-	#print(new_net.bn.get_all_cpts().get(x))
-	frame = new_net.bn.get_all_cpts().get(x)
-	#print(new_net.bn.get_all_cpts().keys())
-	print(frame)
-	#if 'I' in new_net.bn.get_all_cpts().keys():
-		#print('yes')
-	#print('\n')
-
-test_Q = []
-test_Q.append('O')
-test_E = []
-test_E.append('Y')
-test_E.append('X')
-
-def chain_rule(all_tables, query_list):
-
-	tables = new_net.bn.get_all_cpts()
+	tables = new_net.get_all_cpts()
 	
-	number_of_variables = len(new_net.bn.get_all_cpts())
+	number_of_variables = len(new_net.get_all_cpts())
 	num_rows = 2**number_of_variables
 
-	variables = []
-			
-	for key in new_net.bn.get_all_cpts().keys():
-	 	variables.append(key)
+	global variables
+
+	sum_out_variables = deepcopy(variables)
+	for x in query_list:
+		if x in sum_out_variables:
+			sum_out_variables.remove(x)
 	
+	num_rows = 2**(len(variables))
 	variables.append('p')
-	num_rows = 32
 	full_table = pd.DataFrame(index = range(num_rows), columns = variables)
 	
 	for x in range(len(variables)-1):
@@ -136,64 +114,74 @@ def chain_rule(all_tables, query_list):
 					full_table.iloc[y][x] = True
 
 
-	#test_list = list(full_table.iloc[0])
-	#print(test_list)
-
 	for a in range(num_rows):
 		value_list = list(full_table.iloc[a])
 		value_list.remove(np.nan)
 		print(value_list)
 		probability = 1
 
-		for x in new_net.bn.get_all_cpts():
-			#print("x: ", x)
-			current_cpt = new_net.bn.get_all_cpts().get(x)
+		for x in new_net.get_all_cpts():
+
+			current_cpt = new_net.get_all_cpts().get(x)
 			ndf = current_cpt[current_cpt[x] == full_table[x].iloc[a]]
 			
-			#print("shape: ", ndf.shape)
 			if ndf.shape[0] == 1:
 
 				probability *= ndf.iloc[0][ndf.shape[1]-1]
-				#print('probability',probability)
-				
 
 			else:
 				while ndf.shape[0] > 1:
 					for y in variables:
-						#print('y: ',y)
-						#print('value: ',full_table[y].iloc[a])
-						#print('index',ndf.co)
 						if y in ndf.columns:
-							#print('here')
+
 							newdf = ndf[ndf[y] == full_table[y].iloc[a]]
-							#print(ndf)
-							#print(ndf.shape)
 							if newdf.shape == ndf.shape:
 								pass
 							else:
 								ndf=newdf
 								break
-						#print(ndf)
+
 				probability *= ndf.iloc[0][ndf.shape[1]-1]
-				#print('probability',probability)
+
 		full_table['p'].iloc[a] = probability			
 
-			
-				
+
+	#probless_table = full_table.drop(columns=['p'])
+
+	for x in sum_out_variables:
+		full_table = full_table.drop(columns=[x])	  #Remove the first variable to sum out
+		probless_table = full_table.drop(columns=['p'])   #Create a table without probabilities for comparison
+
+		for y in range(num_rows):			  #Loop through all rows
+			for z in range(num_rows):                 #For each row, loop through all rows
+				if y != z and probless_table.iloc[y].equals(probless_table.iloc[z]): #If not looking at the same row, and the rows are the same
+					full_table['p'][y] += full_table['p'][z]		     #Sum the probabilities of the two rows
+					full_table = full_table.drop([z])			     #Remove the lower row from the table
+					break
+			if full_table.shape[0] == num_rows/2:
+				num_rows = int(num_rows/2)
+				break	
+					
+	probless_table = probless_table.drop([1])
 	print(full_table)
-	s = full_table['p']
-	s = s.sum()
-	print(s)
-		
-		
 
+	return full_table
+
+def elimination(query_variables, evidence_variables, ordered_variables):
+	print('NOT DONE YET')
+
+def main():
+
+	test_Q = ['X']
+	test_E = []
+
+	new_net.draw_structure()
+
+	for key in new_net.get_all_cpts().keys():
+		variables.append(key)
 	
+	marginalise(test_Q, test_E)
 
-chain_rule(new_net.bn.get_all_cpts(), test_Q)
-#marginalise(test_Q, test_E)
-
-
-	
-#print(new_net.bn.get_all_cpts().values())
+main()
 
 	
